63919e730d0823a783a0fd753538e17471e4e9d4
keRogers10 <r.murara@alustudent.com>
2023-08-18 14:57:31 -0700

    running 2

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,10 +1,5 @@
 -- Lists all previlages of users
-MYSQL_USER="root"
-MYSQL_PASSWORD="nellynello@138712" 
-mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SHOW GRANTS FOR 'user_0d_1'@'localhost';"
-mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SHOW GRANTS FOR 'user_0d_2'@'localhost';"
-mysql -u root -p nellynello@138712 ; 
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
+mysql -u root -p nellynello@138712  
 source 0-privileges.sql
-
-
-
