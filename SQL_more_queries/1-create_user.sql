60c09660fa7ddf4ac283123721ee801b36978d83
keRogers10 <117177604+keRogers10@users.noreply.github.com>
2023-08-18 13:41:04 -0700

    Update 1-create_user.sql
    run

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,7 +1,3 @@
-
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-   ON .
-   TO 'user_0d_1'@'localhost';
+-- A script that creates the MYSQL server user.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON . TO 'user_0d_1'@'localhost';
