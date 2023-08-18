709079ecb82c2acd429dfedb2b4c5fb4de62e644
keRogers10 <r.murara@alustudent.com>
2023-08-18 14:54:31 -0700

    running 2

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,9 +1,10 @@
-#!/bin/bash
-
+-- Lists all previlages of users
 MYSQL_USER="root"
-MYSQL_PASSWORD="nellynello@138712"  
-
-# List privileges for user_0d_1
+MYSQL_PASSWORD="nellynello@138712" 
 mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SHOW GRANTS FOR 'user_0d_1'@'localhost';"
-# List privileges for user_0d_2
 mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SHOW GRANTS FOR 'user_0d_2'@'localhost';"
+mysql -u root -p nellynello@138712 ; 
+source 0-privileges.sql
+
+
+
