cfc56aae6de70f01e3951ef3abb330953d38043f
keRogers10 <r.murara@alustudent.com>
2023-08-18 14:43:21 -0700

    running 2

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -3,3 +3,13 @@ SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
 mysql -u root -p nellynello@138712  
 source 0-privileges.sql
+#!/bin/bash
+
+MYSQL_USER="root"  # Change this to your MySQL username
+MYSQL_PASSWORD="nellynello@138712"  
+
+# List privileges for user_0d_1
+mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SHOW GRANTS FOR 'user_0d_1'@'localhost';"
+
+# List privileges for user_0d_2
+mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SHOW GRANTS FOR 'user_0d_2'@'localhost';"
